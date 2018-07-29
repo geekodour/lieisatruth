51ede4324e2cab933205ca724c442e1096619b76
Hrishikesh Barman <hrishikeshbman@gmail.com>
2018-07-29 09:15:20 +0530

    wip

--- src/utils.js
+++ src/utils.js
@@ -12,26 +12,53 @@ export const generateTweet = (name, username, text, image_url, container_id) => 
         '20',
         document.getElementById(container_id)
     ).then((e)=>{
-        const screenName = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
-                .shadowRoot
-                .querySelector('.TweetAuthor-screenName.Identity-screenName')
 
-        const fullname = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
-                .shadowRoot
-                .querySelector('.TweetAuthor-name.Identity-name')
+        try {
+            const screenName = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
+                    .shadowRoot
+                    .querySelector('.TweetAuthor-screenName.Identity-screenName')
 
-        const tweet = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
-                .shadowRoot
-                .querySelector('p.Tweet-text.e-entry-title')
+            const fullname = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
+                    .shadowRoot
+                    .querySelector('.TweetAuthor-name.Identity-name')
 
-        const avatar = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
-                .shadowRoot
-                .querySelector('img.Avatar.Avatar--edge')
+            const tweet = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
+                    .shadowRoot
+                    .querySelector('p.Tweet-text.e-entry-title')
 
-        // change stuff
-        screenName.innerText = username
-        fullname.innerText = name
-        tweet.innerText = text
-        avatar.src = image_url
+            const avatar = document.querySelector(`#${container_id} .twitter-tweet.twitter-tweet-rendered`)
+                    .shadowRoot
+                    .querySelector('img.Avatar.Avatar--edge')
+
+            // change stuff
+            screenName.innerText = username
+            fullname.innerText = name
+            tweet.innerText = text
+            avatar.src = image_url
+        } catch(err){
+            // other browsers
+            const screenName = document.querySelector(`#${container_id} iframe`)
+                    .contentWindow.document
+                    .querySelector('.TweetAuthor-screenName.Identity-screenName')
+
+            const fullname = document.querySelector(`#${container_id} iframe`)
+                    .contentWindow.document
+                    .querySelector('.TweetAuthor-name.Identity-name')
+
+            const tweet = document.querySelector(`#${container_id} iframe`)
+                    .contentWindow.document
+                    .querySelector('p.Tweet-text.e-entry-title')
+
+            const avatar = document.querySelector(`#${container_id} iframe`)
+                    .contentWindow.document
+                    .querySelector('img.Avatar.Avatar--edge')
+
+            // change stuff
+            screenName.innerText = username
+            fullname.innerText = name
+            tweet.innerText = text
+            avatar.src = image_url
+        }
+
     });
 }
